7f18572ccf2014ac0dabcafc312818f9e8359de2
RealmOFWeebs <siddharth.075@proton.me>
2024-09-04 09:07:39 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/TheRealmOfWeebs'>This Channel</a>\n○ Movies Channel : <a href='https://t.me/RealmOfBlockbusters'>Realm of Blockbusters</a>\n○ Support Bot : <a href='https://t.me/TheRealmOfWeebs'>Contact Us</a></b>",
+            text = f"<b>○ Creator : <a href='https://t.me/TheRealmOfWeebs'>This Channel</a>\n○ Movies Channel : <a href='https://t.me/RealmOfBlockbusters'>Realm of Blockbusters</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
